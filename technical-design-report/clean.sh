bcdd1e851deb03f195fb5001c33795fec7f0daa1
eloy <eloymaduro@gmail.com>
2019-01-22 12:52:29 +0100

    final grammar checks, Abbreviations corrected

--- technical-design-report/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm technical-design-report.bbl
-rm technical-design-report.aux
-rm technical-design-report.log
-rm technical-design-report.toc
-rm technical-design-report.out
-rm technical-design-report.blg
